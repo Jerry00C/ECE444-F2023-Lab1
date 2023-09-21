6a7b694aa3678c0bd8c13855b022304fd9260ddc
Jerry123455 <JiangShan.Cui18@ycdsbk12.ca>
2023-09-20 21:04:06 -0400

    Commit to Activity 4 for reproducing example 4-7

--- hello.py
+++ hello.py
@@ -1,16 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'HARDTOGUESSES'
 moment = Moment(app)
 bootstrap = Bootstrap(app)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    print(session.get('name'))
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        old_info = session.get('info')
+        if old_info and old_info['name'] is not None and old_info['name'] != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['info'] = {
+            'name': form.name.data,
+        }
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, formInfo=session.get('info'))
 
 
 @app.route('/user/<name>')
